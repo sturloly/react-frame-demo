3e2f9a1e54a61d8c8b74db70023c289683ac3d0d
sturloly <sturloly@gmail.com>
2017-07-10 22:54:43 +0800

    course container view

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,34 @@
 import React, {ProTypes} from 'react';
 
 class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: { title: null }
+    };
+  }
+
+  onTitleCHange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course });
+  }
+
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title} />
+
+        <input
+          type="submit"
+          value={this.state.course.title}
+          onClick={this.onClickSave} />
       </div>
     );
   }
